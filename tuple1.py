8ba2e95007cf05fea9699f5d92e4ab176f6d2595
kaushal2222 <158256664+kaushal2222@users.noreply.github.com>
2024-03-21 11:03:54 +0530

    Create tuple1.py

---
+++ tuple1.py
@@ -0,0 +1,56 @@
+internet = ("cern", "timbernerslee", "www", 1980)
+print(internet)
+print(type(internet))
+
+air_force = ("f15", "f22a", "f35a")
+fighter_jets = (1988, 2005, 2016, air_force)
+print(fighter_jets)
+
+empty=()
+the='hello',
+print(the)  
+
+tuple_1 = (2, 0, 1, 5)
+tuple_2 = (2, 0, 1, 9)
+if tuple_1<tuple_2:
+
+    print("ture")
+else:
+    print("false")
+
+if tuple_1 != tuple_2:
+    print("true")
+a=tuple_1+tuple_2
+b=tuple_1*3
+print(a)
+print(b)
+if tuple_1==tuple_2:
+    print("true")
+else:
+    print("false")
+
+tupel1=(1,2,3,4,5)
+if 1 in tupel1:
+    print("true")
+else:
+    print("false")
+
+norse = "vikings"
+converttotuple = tuple(norse)
+a=converttotuple+tuple("kaushal")
+print(a)
+print(converttotuple)
+
+Pavandev = ["g", "o", "d", "o", "f", "s", "k", "y"]
+listtotuple=tuple(Pavandev)
+a=listtotuple+ tuple(["g", "r", "e", "e", "k"])
+print(a)
+print(listtotuple)
+
+letters = ("a", "b", "c")
+numbers = (1, 2, 3)
+nested_tuples = (letters, numbers)
+print(nested_tuples)
+
+tup=tuple("wolverine")
+print(tup)
